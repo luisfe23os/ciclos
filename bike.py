3b97cf7538ae97f6678d94d7ab0f831b4742a480
juanma <juanmitha777@outlook.com>
2023-08-25 11:15:51 -0500

    se realiza ejercicio de ciclos

---
+++ bike.py
@@ -0,0 +1,23 @@
+bike=["biela","pedales","sillin"]
+opcion=0
+print("partbikes")
+print(".............")
+print("1.agregar producto")
+print("2.ver productos de bodega")
+print("3.obtener valor del inventario")
+print("4.ver los productos mas vendidos")
+print("5.salir")
+while opcion!=5:
+    opcion=int(input("digita un numero"))
+
+    if opcion==1:
+        print("opcion 1")
+    elif opcion==2:
+        print("opcion2")
+    elif opcion==3:
+        print("opcion 3")
+    elif opcion==4:
+        print("opcion 4")
+    else:
+        print("error")
+print("salio del programa")
